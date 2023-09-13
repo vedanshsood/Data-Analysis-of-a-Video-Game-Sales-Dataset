d32c7e4e0e4e55cee62f3b1395b7ee3cde2ee9fe
vedanshsood <vsood0071@gmail.com>
2023-09-13 22:19:44 +0530

    Update task2.py

--- task2.py
+++ task2.py
@@ -2,31 +2,56 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+
+#Pearson Correlation Coefficient
+def pearson(x,y):
+    pearson = np.corrcoef(x,y)
+    return pearson
 
 
 # Reading the csv file
 data=pd.read_csv("Tasks Data Science\Games_dataset.csv")
 data.info()
-data.shape
-data.describe().T
+data = pd.DataFrame(data)
 
-#Mean
-mean=data["Global_Sales"].mean()
-#Median
-median=data["Global_Sales"].median()
-#Mode
-mode=data["Global_Sales"].mode()
-#Standard Deviation
-dev=np.std(data["Global_Sales"])
-# Printing the desprictive Analysis
-print("Mean: ", mean)
-print("Median: ", median)
-print("Mode: ", mode)
-print("Standard Deviation: ",dev )
+#printing data
+print(data)
+
+# Descriptive Analysis of the data 
+print(data.describe())
+
 
 #Creating Histogram
 fig, ax = plt.subplots(figsize =(10, 7))
-ax.hist(data["Global_Sales"], bins = [0, 8, 16, 24, 32, 40, 48, 56, 64])
+ax.hist(data["Global_Sales"], bins = [0, 8, 16, 24, 32, 40, 48, 56])
 
 #Showing the plot
+plt.show()
+
+#Calculating Pearson correlation coefficient for North America Sales and Global Sales
+correlation = pearson(data["NorthAmerica_Sales"],data['Global_Sales'])
+print("The Pearson Correlation Coefficient is: ")
+print(correlation)
+
+
+# Finding Null values in the data 
+print(data.isnull().sum())
+
+#Removing string columns
+data1 = data.drop(["Name","Platform","Year" , "Genre","Publisher"] , axis = 1)
+data1.head()
+
+# Correlation matrix
+relation = data1.corr()
+sns.heatmap(relation, xticklabels=relation.columns , yticklabels=relation.columns , annot=True)
+
+plt.show()
+
+#Data visualisation for the sales of video games
+sns.pairplot(relation)
+plt.show()
+
+#Scatter plot
+sns.relplot(x="NorthAmerica_Sales", y="Japan_Sales", hue="Genre", data= data)
 plt.show()
